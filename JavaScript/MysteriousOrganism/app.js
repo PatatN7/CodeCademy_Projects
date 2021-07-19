a476083fb1ac2668bac8bfcaa527ff88e3028fe4
PatatN7 <jmeintjesn7@gmail.com>
2021-07-19 22:21:18 +0200

    Strand spelling correction

--- JavaScript/MysteriousOrganism/app.js
+++ JavaScript/MysteriousOrganism/app.js
@@ -4,7 +4,7 @@ const returnRandBase = () => {
     return dnaBases[Math.floor(Math.random() * 4)];
 };
 
-// Returns a random single stand of DNA containing 15 bases
+// Returns a random single strand of DNA containing 15 bases
 const mockUpStrand = () => {
     const newStrand = [];
     for (let i = 0; i < 15; i++) {
